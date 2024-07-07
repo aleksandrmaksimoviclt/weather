947561b4a46f84b4d685048979195243ec1341b3
Aleksandr <aleksandr.maksimoviclt@gmail.com>
2024-07-07 17:31:02 +0200

    feat: add env configuration support for frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    // API_KEY: process.env.API_KEY,
+    // DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
 
 export default nextConfig;
