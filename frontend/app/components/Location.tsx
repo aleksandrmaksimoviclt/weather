6302169e12670072013cc49aebe1857d561819b6
Aleksandr <aleksandr.maksimoviclt@gmail.com>
2024-07-08 15:11:48 +0200

    feat: Add placeholder component for displaying current location that is used for weather information fetching

---
+++ frontend/app/components/Location.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Location: React.FC = () => {
+  return (
+    <div className="flex flex-row items-center">
+      <p>Location</p>
+    </div>
+  );
+};
+
+export default Location;
