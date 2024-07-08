92870eb7df2193c2489afeb52dcff9cec28f5b6a
Aleksandr <aleksandr.maksimoviclt@gmail.com>
2024-07-08 15:11:20 +0200

    feat: Add placeholder component for displaying when was last update of weather done

---
+++ frontend/app/components/LastUpdated.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LastUpdated: React.FC = () => {
+  return (
+    <div className="flex flex-row justify-end w-full">
+      <p>Last updated: </p> <p>20s</p>
+    </div>
+  );
+};
+
+export default LastUpdated;
