3569425489d42acfc599e2f37666327d503ee7d4
Aleksandr <aleksandr.maksimoviclt@gmail.com>
2024-07-08 15:11:05 +0200

    feat: Add placeholder component for displaying current weather

---
+++ frontend/app/components/CurrentWeather.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CurrentWeather: React.FC = () => {
+  return (
+    <div className="flex flex-row items-center">
+      <div>Weather icon</div>
+      <div>Weather degrees</div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
